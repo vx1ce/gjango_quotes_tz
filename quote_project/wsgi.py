a3eeed71006235336b505115a906f9e5711e9937
vx1ce <95540545+vx1ce@users.noreply.github.com>
2025-09-09 23:58:06 +0200

    Update wsgi.py

--- quote_project/wsgi.py
+++ quote_project/wsgi.py
@@ -1,16 +1,14 @@
-"""
-WSGI config for quote_project project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
-"""
-
 import os
+import sys
 
+# Добавьте путь к вашему проекту
+path = '/home/вашusername/django-quotes-app'
+if path not in sys.path:
+    sys.path.append(path)
+
+# Настройте переменные окружения
+os.environ['DJANGO_SETTINGS_MODULE'] = 'quote_project.settings'
+
+# Импортируйте и настройте Django
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'quote_project.settings')
-
 application = get_wsgi_application()
